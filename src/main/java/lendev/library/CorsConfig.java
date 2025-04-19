0125a1f45518f1df524f0a24a5d6290b60101544
root <root@4536-NB>
2025-04-19 10:21:48 -0300

    fix: config cors

--- src/main/java/lendev/library/CorsConfig.java
+++ src/main/java/lendev/library/CorsConfig.java
@@ -1,4 +1,23 @@
 package lendev.library;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // libera todas as rotas
+                        .allowedOrigins("*") // ou coloque a origem específica do seu Swagger UI
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
